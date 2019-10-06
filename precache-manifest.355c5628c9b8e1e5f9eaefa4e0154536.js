edd7781f6819048182ee7695d2b606205cf2f223
Bharath <bharath@Bharaths-MacBook-Air.local>
2019-10-06 10:33:19 +0530

    Updates

---
+++ precache-manifest.355c5628c9b8e1e5f9eaefa4e0154536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7caf27256085e4052ae7c2bc9337413c",
+    "url": "/Mini_Online_Store/index.html"
+  },
+  {
+    "revision": "236ee1861f77e5f9285d",
+    "url": "/Mini_Online_Store/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "21a53f10c9c7a33dd976",
+    "url": "/Mini_Online_Store/static/css/main.20fb29a8.chunk.css"
+  },
+  {
+    "revision": "236ee1861f77e5f9285d",
+    "url": "/Mini_Online_Store/static/js/2.efd7c42d.chunk.js"
+  },
+  {
+    "revision": "21a53f10c9c7a33dd976",
+    "url": "/Mini_Online_Store/static/js/main.b7547ac0.chunk.js"
+  },
+  {
+    "revision": "b20ccc26a30840fe3c99",
+    "url": "/Mini_Online_Store/static/js/runtime~main.7458aed7.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Mini_Online_Store/static/media/logo.9f7e506d.svg"
+  }
+]);
